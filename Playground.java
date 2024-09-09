f5fce365ce1b30243bfa9740698655c147234483
r567tw <r567tw@gmail.com>
2024-09-10 07:49:21 +0800

    some try

--- Playground.java
+++ Playground.java
@@ -1,7 +1,15 @@
 
+
 class Playground {
 
     public static void main(String[] args) {
+
+        // Scanner input = new Scanner(System.in);
+        // int Num = input.nextInt();
+        // System.out.println(Num);
+        System.out.println(System.currentTimeMillis());
+
+        System.out.println(6 / 3);
         System.out.println(6.0 / 3);
         System.out.println("Hello Here're Playground");
     }
