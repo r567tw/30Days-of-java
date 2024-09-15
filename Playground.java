e469dab4ec27fa2439e7b7ef7cf2afd48158b25b
r567tw <r567tw@gmail.com>
2024-09-16 07:08:11 +0800

    play

--- Playground.java
+++ Playground.java
@@ -12,5 +12,14 @@ class Playground {
         System.out.println(6 / 3);
         System.out.println(6.0 / 3);
         System.out.println("Hello Here're Playground");
+
+        int x = 1;
+        int y = -1;
+        int z = 1;
+        if (x > 0)
+        if (y > 0)
+            System.out.println("x > 0 and y > 0");
+        else if (z > 0)
+            System.out.println("aaa x < 0 and z > 0");
     }
 }
