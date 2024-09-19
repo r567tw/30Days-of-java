646520a54cf44f71781d4203dfcc517214b24f18
r567tw <r567tw@gmail.com>
2024-09-19 08:07:38 +0800

    Day 04

---
+++ Day04/CharGame.java
@@ -0,0 +1,9 @@
+class CharGame {
+    public static void main(String[] args) {
+        char character = 'a';
+
+        System.out.println(character);
+        System.out.println((char) 97);
+        System.out.println(character + 1);    
+    }
+}
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 - Day01: HelloWorld
 - Day02: Covert Temperature
 - Day03: Math
+- Day04: Character
